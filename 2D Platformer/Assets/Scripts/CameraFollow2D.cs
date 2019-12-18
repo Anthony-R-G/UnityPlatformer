795e55bd8db00ac221099e8a70e5844c7ef46bd4
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-12-17 19:05:55 -0500

    Initial commit

---
+++ 2D Platformer/Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector2 posOffset;
+
+    [SerializeField]
+    float leftLimit;
+
+    [SerializeField]
+    float rightLimit;
+
+    [SerializeField]
+    float bottomLimit;
+
+    [SerializeField]
+    float topLimit;
+
+
+    private Vector3 velocity;
+
+    void Update()
+    {
+        //Cameras current position
+        Vector3 startPos = transform.position;
+
+        //Players current position
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        //Linear Interpolation (Lerp)
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        //dampening
+        //transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffset);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Top Boundary Line
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+
+        //Right Boundary Line
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+
+        //Bottom Boundary Line
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+
+        //Left Boundary Line
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+
+    }
+}
---
+++ 2D Platformer/Assets/Scripts/playerController2D.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class playerController2D : MonoBehaviour
+{
+
+    Animator animator;
+
+    Rigidbody2D rb2d;
+
+    SpriteRenderer spriteRenderer;
+
+   [SerializeField]
+    private float walkSpeed = 6;
+
+    [SerializeField]
+    private float jumpHeight = 5;
+
+    public Transform groundCheck;
+    public Transform groundCheck_L;
+    public Transform groundCheck_R;
+
+    bool isGrounded;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")) ||
+           (Physics2D.Linecast(transform.position, groundCheck_L.position, 1 << LayerMask.NameToLayer("Ground")))||
+           (Physics2D.Linecast(transform.position, groundCheck_R.position, 1 << LayerMask.NameToLayer("Ground"))))
+        {
+            isGrounded = true;
+        
+        } else {
+            
+            isGrounded = false;
+            animator.Play("Player_jump");
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(walkSpeed, rb2d.velocity.y);
+
+            if (isGrounded == true)
+            {
+                animator.Play("Player_walk");
+            }
+
+            spriteRenderer.flipX = false;
+
+        } else if (Input.GetKey("a") || Input.GetKey("left")) {
+
+            rb2d.velocity = new Vector2(-walkSpeed, rb2d.velocity.y);
+
+            if (isGrounded == true)
+            {
+                animator.Play("Player_walk");
+            }
+
+            spriteRenderer.flipX = true;
+
+        } else {
+            if (isGrounded == true)
+            {
+                animator.Play("Player_idle");
+            }
+
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+
+        if (Input.GetKey("space") && isGrounded == true)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
+            animator.Play("Player_jump");
+        }
+
+        
+
+    }
+}
